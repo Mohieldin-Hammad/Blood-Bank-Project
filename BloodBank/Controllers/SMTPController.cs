867b6cf1f859cef28432c175a42d3f5c219e55a7
Mohieldin-Hammad <97237706+Mohieldin-Hammad@users.noreply.github.com>
2022-05-28 20:09:07 +0200

    adding sending email feature

---
+++ BloodBank/Controllers/SMTPController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net.Mail;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
+
+namespace BloodBank.Controllers
+{
+    class SMTPController
+    {
+        private string EmailBody = "hello, mohee your code is successfully worked";
+        private string subject = "Trying to write the code";
+        private string mailTitle = "Done";
+        //-------------
+        //enter your email and password
+        private string senderEmail = "email";
+        private string senderEmailPassword = "password";
+        //-------------
+        public string SendEmailTo(string receiverEmail)
+        {
+            // Email and Content
+            MailMessage mailMessage = new MailMessage(new MailAddress(senderEmail, mailTitle), new MailAddress(receiverEmail));
+            mailMessage.Subject = subject;
+            mailMessage.Body = EmailBody;
+            //mailMessage.IsBodyHtml = true;
+
+
+            // server
+            System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 465;
+            smtp.EnableSsl = true;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+
+            // credentials
+            System.Net.NetworkCredential credentials = new System.Net.NetworkCredential();
+            credentials.UserName = senderEmail;
+            credentials.Password = senderEmailPassword;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = credentials;
+
+            try
+            {
+                smtp.Send(mailMessage);
+                return "Succeed";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+
+        }
+
+    }
+
+
+    class SMTPWithMailKit
+    {
+        //-------------
+        //enter your email and password
+        private string senderEmail = "email";
+        private string senderEmailPassword = "password";
+        //-------------
+        public string SendEmailTo(string email)
+        {
+            MimeMessage message = new MimeMessage();
+            
+            message.From.Add(new MailboxAddress("Tester", "bloodbank354@gmail.com"));
+            message.To.Add(MailboxAddress.Parse(email));
+            message.Subject = "Trying to write the code";
+            message.Body = new TextPart("plain")
+            {
+                Text = "hello, mohee your code is successfully worked"
+            };
+
+            MailKit.Net.Smtp.SmtpClient client = new MailKit.Net.Smtp.SmtpClient();
+            string checkMessage = "";
+            try
+            {
+                client.Connect("smtp.gmail.com", 465, true);
+                client.Authenticate(senderEmail, senderEmailPassword);
+                client.Send(message);
+                checkMessage = "Succeed";
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                client.Disconnect(true);
+                client.Dispose();
+            }
+            return checkMessage;
+        }
+    }
+}
